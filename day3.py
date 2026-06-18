ffb6ccda121f85652d5d853a444e950a26121bd1
Wesley Wanga <wesleyjwanga@gmail.com>
2026-06-18 20:38:04 +0300

    update the files

---
+++ day3.py
@@ -0,0 +1,42 @@
+#logical operators (and, or, not)
+#and operators=> works when all conditions ,ust be true
+score= 80
+attendance= 95
+print(score>=50 and attendance>=90)
+
+#or operator: at least one condition must be true
+score=30
+attendance=100
+print(score>60 or attendance>=85)
+
+# not operator
+is_student_active=True
+print(not is_student_active)
+
+# conditionals ( if, else, elif)
+#if statement
+#if condition:
+#     code_to_run
+age= 16
+if age>=18:
+    print(" this person is an adult")
+
+# important rulr: Indentation
+age= 16
+if age>=18:
+    print(" this person is an adult")
+else: 
+    print("This person is a minor")
+
+#elif 
+# if condition1:
+#     code run if condition1 is true
+# elif condition2: 
+#     code run if condition1 is true
+# elif condition3: 
+#     code run if condition1 is true
+# else:
+#     run if all conditions are false
+    
+    
+
---
+++ grade_checker.py
@@ -0,0 +1,12 @@
+student_name="Alice"
+score= 85
+if score >=50:
+    print(student_name, "got Grade D")
+elif score>=60: 
+    print(student_name, "got Grade C")
+elif score>=70:
+    print(student_name, "got Grade B")
+elif score>=80:
+    print(student_name, "got Grade A")
+else: 
+    print(student_name, "has failed")
