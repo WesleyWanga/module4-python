5fa4cb01af226211efcc89e03d9dc03170a8d18e
Wesley Wanga <wesleyjwanga@gmail.com>
2026-06-19 19:47:27 +0300

    update the files made changes on

---
+++ day4.py
@@ -0,0 +1,40 @@
+# Loops
+# print("Welcome to Python")
+# print("Welcome to Python")
+# print("Welcome to Python")
+# print("Welcome to Python")
+# print("Welcome to Python")
+
+# While and for loops 
+# for loop
+# for item in sequence:
+#     block_of code
+
+# for number in range(10):
+#     print(number)
+
+# for number in range(1,6):
+#                     print(number)
+
+# for number in range(2,11,2):
+#                     print(number)
+
+# students=["John", "Alice", "Steven", "Jesca", "Grace", "Kevin"]
+# for student in students:
+#         print(f"Hello {student}")
+
+# scores=[80, 45, 90, 30, 60]
+# for score in scores:
+#         if score>=50:
+#                 print(score, "passed")
+#         else: 
+#                 print(score, "failed")
+# f"text {variable_name}"
+
+# while condition:
+#         code _to_execute
+
+count= 1
+while count<=10: 
+        print(count)
+        count+=1
